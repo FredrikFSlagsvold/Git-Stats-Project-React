d144a14be1544fd77eafeb922e70150b85728179
Fredrik Faanes Slagsvold <fredrfsl@stud.ntnu.no>
2022-09-21 13:56:46 +0200

    Revert "Im failing"
    This reverts commit f2884977aae98a0489900d0895538e0c2228c4df.

--- project-2/src/utils/axios.ts
+++ project-2/src/utils/axios.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const URL = "https://gitlab.stud.idi.ntnu.no/api/v4/projects/17434"
-const accessToken = "Bearer glpat-ZoJcxHwG6cnKzMSGwFrb"
+const URL = "https://gitlab.stud.idi.ntnu.no/api/v4/projects/17434/"
 
 const acess = "Bearer glpat-2t5FVWJaUJ6uHTjjzWLP"
 const other = "Bearer glpat-g2q1s6_nosxofSqqtsgD"
