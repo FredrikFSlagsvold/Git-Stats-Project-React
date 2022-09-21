f2884977aae98a0489900d0895538e0c2228c4df
Fredrik Faanes Slagsvold <fredrfsl@stud.ntnu.no>
2022-09-21 13:54:38 +0200

    Im failing

--- project-2/src/utils/axios.ts
+++ project-2/src/utils/axios.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const URL = "https://gitlab.stud.idi.ntnu.no/api/v4/projects/17434/"
+const URL = "https://gitlab.stud.idi.ntnu.no/api/v4/projects/17434"
+const accessToken = "Bearer glpat-ZoJcxHwG6cnKzMSGwFrb"
 
 const acess = "Bearer glpat-2t5FVWJaUJ6uHTjjzWLP"
 const other = "Bearer glpat-g2q1s6_nosxofSqqtsgD"
