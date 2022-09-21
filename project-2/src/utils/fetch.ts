1fd90e4abeac1b113ac6f836372661d60eab8b56
Fredrik Faanes Slagsvold <fredrfsl@stud.ntnu.no>
2022-09-21 15:59:28 +0200

    fixed fetch file

--- project-2/src/utils/fetch.ts
+++ project-2/src/utils/fetch.ts
@@ -1,4 +1,4 @@
-import { axiosInstance } from "./axios"
+import axiosInstance from "./axios"
 
 export async function getMergeRequests() {
     const mergeRequests = await axiosInstance.get("/merge_requests");
