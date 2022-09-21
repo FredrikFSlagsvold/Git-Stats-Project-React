74ee136e22f0abe8d630e2f9e64eb299fa30427f
Fredrik Faanes Slagsvold <fredrfsl@stud.ntnu.no>
2022-09-21 14:12:19 +0200

    changed axios-file

--- project-2/src/utils/axios.ts
+++ project-2/src/utils/axios.ts
@@ -3,10 +3,12 @@ import axios from "axios";
 const URL = "https://gitlab.stud.idi.ntnu.no/api/v4/projects/17434"
 const accessToken = "Bearer glpat-ZoJcxHwG6cnKzMSGwFrb"
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: URL,
     timeout: 3000,
     headers: {
         Authorization: accessToken
     }
 })
+
+export default axiosInstance;
