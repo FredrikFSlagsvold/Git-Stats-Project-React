589275ee1338fe0956f2d8a25aea45f44f20587b
Fredrik Faanes Slagsvold <fredrfsl@stud.ntnu.no>
2022-09-23 13:10:33 +0200

    yolo

--- project-2/src/utils/Page.ts
+++ project-2/src/utils/Page.ts
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { getMergeRequests } from './utils/fetch';
 
 type Issue = {
     id: string;
 }
 
-export default Page;
+
+export default function Page(){
+    const [mergeRequest, setMergeRequest] = useState<Issue[]>([]);
+
+    getMergeRequests()
+}
